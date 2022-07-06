e1b66b63ca44cf297b38988d73dd59ab213c35ce
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-06 21:44:06 +0300

    corrected

--- js-full/dom/5/index.js
+++ js-full/dom/5/index.js
@@ -1,7 +1,5 @@
-export function setTitle(text) {
+function setTitle(text) {
   const element = document.querySelector(".title");
   element.textContent = text;
   return text;
 }
-
-setTitle("Some new text");
