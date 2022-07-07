f21fd52b5cf39e695740ab9cce8736f4dc733161
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-07 15:58:19 +0300

    added func

---
+++ js-full/dom/11/index.js
@@ -0,0 +1,14 @@
+export function manageClasses() {
+  const classOne = document.querySelector(".one");
+  classOne.classList.add("selected");
+  const classTwo = document.querySelector(".two");
+  classTwo.classList.remove("selected");
+  const classThree = document.querySelector(".three");
+  classThree.classList.toggle("three_done");
+  const classFour = document.querySelector(".four");
+  if (classFour.classList.contains("some-class")) {
+    classFour.classList.add("another-class");
+  }
+
+  return classOne, classTwo, classThree, classFour;
+}
