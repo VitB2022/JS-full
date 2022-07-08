1365d2a4322867a9887fa59bfba360347d250f47
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-08 12:08:31 +0300

    added handler to attach btn

--- js-full/events/index.js
+++ js-full/events/index.js
@@ -41,4 +41,18 @@ function removeEventHandler() {
   spanElem.removeEventListener("click", logGreySpan, true);
   spanElem.removeEventListener("click", logGreenSpan);
 }
+
 buttonRemove.addEventListener("click", removeEventHandler);
+
+const buttonAttach = document.querySelector(".attach-handlers-btn");
+
+function attachEventHandler() {
+  divElem.addEventListener("click", logGreyDiv, true);
+  divElem.addEventListener("click", logGreenDiv);
+  pElem.addEventListener("click", logGreyP, true);
+  pElem.addEventListener("click", logGreenP);
+  spanElem.addEventListener("click", logGreySpan, true);
+  spanElem.addEventListener("click", logGreenSpan);
+}
+
+buttonAttach.addEventListener("click", attachEventHandler);
