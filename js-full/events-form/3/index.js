c14ca18f08356cbfc35cf7b6c2b79c908713d562
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-11 11:17:39 +0300

    added data to func and corrected code

--- js-full/events-form/3/index.js
+++ js-full/events-form/3/index.js
@@ -1,11 +1,26 @@
 'use strict';
 
 const tasks = [
-  { text: 'Buy milk', done: false },
-  { text: 'Pick up Tom from airport', done: false },
-  { text: 'Visit party', done: false },
-  { text: 'Visit doctor', done: true },
-  { text: 'Buy meat', done: true },
+  {
+    text: 'Buy milk',
+    done: false,
+  },
+  {
+    text: 'Pick up Tom from airport',
+    done: false,
+  },
+  {
+    text: 'Visit party',
+    done: false,
+  },
+  {
+    text: 'Visit doctor',
+    done: true,
+  },
+  {
+    text: 'Buy meat',
+    done: true,
+  },
 ];
 
 const listElem = document.querySelector('.list');
@@ -13,23 +28,21 @@ const listElem = document.querySelector('.list');
 const renderTasks = tasksList => {
   const listItemsElems = tasksList
     .sort((a, b) => a.done - b.done)
-    .map(({ text, done }) => {
+    .map(({ text, done }, i) => {
       const listItemElem = document.createElement('li');
       listItemElem.classList.add('list__item');
       if (done) {
         listItemElem.classList.add('list__item_done');
       }
-
       const checkboxElem = document.createElement('input');
       checkboxElem.setAttribute('type', 'checkbox');
       checkboxElem.checked = done;
+      checkboxElem.dataset.i = i;
       checkboxElem.classList.add('list__item-checkbox');
-
       listItemElem.append(checkboxElem, text);
-
       return listItemElem;
     });
-
+  listElem.innerHTML = '';
   listElem.append(...listItemsElems);
 };
 
@@ -39,39 +52,29 @@ const clickButton = document.querySelector('.create-task-btn');
 
 const handlerButton = () => {
   const getTask = document.querySelector('.task-input');
-  if (getTask.value !== '') {
-    tasks.push({ text: getTask.value, done: false });
-
-    const getListItemElem = document.createElement('li');
-    getListItemElem.classList.add('list__item');
-    getListItemElem.innerText = getTask.value;
-
-    const getCheckboxElem = document.createElement('input');
-    getCheckboxElem.setAttribute('type', 'checkbox');
-    getCheckboxElem.classList.add('list__item-checkbox');
-
-    listElem.prepend(getListItemElem);
-    getListItemElem.prepend(getCheckboxElem);
+  let text = getTask.value;
+  if (text) {
+    tasks.push({
+      text,
+      done: false,
+    });
     getTask.value = '';
+
+    renderTasks(tasks);
+    // console.log(tasks);
   }
 };
 
 clickButton.addEventListener('click', handlerButton);
 
-const handlerChexbox = () => {
-  const lis = Array.from(listElem.querySelectorAll('.list__item'));
-  const before = [],
-    after = [];
-  for (let li of lis) {
-    if (li.querySelector(':checked')) {
-      li.classList.add('list__item_done');
-      after.push(li);
-    } else {
-      li.classList.remove('list__item_done');
-      before.push(li);
-    }
-  }
+const handlerChexbox = ({
+  target: {
+    dataset: { i },
+  },
+}) => {
+  tasks[i].done = !tasks[i].done;
 
-  listElem.append(...before, ...after);
+  renderTasks(tasks);
 };
-listElem.addEventListener('click', handlerChexbox);
+
+listElem.addEventListener('change', handlerChexbox);
