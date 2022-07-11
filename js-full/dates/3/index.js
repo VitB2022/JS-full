a8fb8eaf0cf44b3672130c4cc18dc1cacfcdb64a
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-11 20:05:23 +0300

    corrected

--- js-full/dates/3/index.js
+++ js-full/dates/3/index.js
@@ -2,10 +2,10 @@ export const getDiff = (startDate, endDate) => {
   let date1 = new Date(startDate);
   let date2 = new Date(endDate);
 
-  let days = parseInt(Math.ceil(Math.abs((date1 - date2) / (1000 * 60 * 60 * 24))));
-  let hours = parseInt(Math.ceil(Math.abs((date1 - date2) / (1000 * 60 * 60))));
-  let minutes = parseInt(Math.ceil(Math.abs((date1 - date2) / (1000 * 60))));
-  let seconds = parseInt(Math.ceil(Math.abs((date1 - date2.getSeconds()) / 1000)));
+  let days = Math.round(Math.abs((date1 - date2) / (1000 * 60 * 60 * 24)));
+  let hours = Math.round(Math.abs((date1 - date2) / (1000 * 60 * 60)));
+  let minutes = Math.round(Math.abs((date1 - date2) / (1000 * 60)));
+  let seconds = Math.round(Math.abs((date1 - date2.getSeconds()) / 1000));
 
   return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 };
