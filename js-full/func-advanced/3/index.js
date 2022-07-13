43a5e8bd43ebd06c659d28bdb51a7e0d35823eaa
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-13 22:38:59 +0300

    corrected

--- js-full/func-advanced/3/index.js
+++ js-full/func-advanced/3/index.js
@@ -2,10 +2,10 @@ export const mult = a => b => a * b;
 
 // console.log(mult(5)(4));
 
-export const twice = mult => mult * 2;
+export const twice = mult(2);
 
 // console.log(twice(7));
 
-export const triple = mult => mult * 3;
+export const triple = mult(3);
 
 // console.log(triple(9));
