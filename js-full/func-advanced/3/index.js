529d675208016bf3e22bdfb5da017bed46766890
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-13 22:30:32 +0300

    added functions

---
+++ js-full/func-advanced/3/index.js
@@ -0,0 +1,11 @@
+export const mult = a => b => a * b;
+
+// console.log(mult(5)(4));
+
+export const twice = mult => mult * mult;
+
+// console.log(twice(7));
+
+export const triple = mult => mult * 3;
+
+// console.log(triple(9));
