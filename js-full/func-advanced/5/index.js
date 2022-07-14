d381f63bfd3c4a31b3eac6976a17ecfd24ad75dd
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-14 19:19:24 +0300

    corrected time format

--- js-full/func-advanced/5/index.js
+++ js-full/func-advanced/5/index.js
@@ -1,29 +1,29 @@
 export const shmoment = initValue => {
-  let resultDate = initValue;
+  initValue = new Date(initValue);
 
   const newValue = {
     add(dateInterval, number) {
       switch (dateInterval) {
         case 'years':
-          resultDate = initValue.setFullYear(initValue.getFullYear() + number);
+          initValue.setFullYear(initValue.getFullYear() + number);
           break;
         case 'months':
-          resultDate = initValue.setMonth(initValue.getMonth() + number);
+          initValue.setMonth(initValue.getMonth() + number);
           break;
         case 'days':
-          resultDate = initValue.setDate(initValue.getDay() + number);
+          initValue.setDate(initValue.getDay() + number);
           break;
         case 'hours':
-          resultDate = initValue.setHours(initValue.getHours() + number);
+          initValue.setHours(initValue.getHours() + number);
           break;
         case 'minutes':
-          resultDate = initValue.setMinutes(initValue.getMinutes() + number);
+          initValue.setMinutes(initValue.getMinutes() + number);
           break;
         case 'seconds':
-          resultDate = initValue.setSeconds(initValue.getSeconds() + number);
+          initValue.setSeconds(initValue.getSeconds() + number);
           break;
         case 'milliseconds':
-          resultDate = initValue.setMilliseconds(initValue.getMilliseconds() + number);
+          initValue.setMilliseconds(initValue.getMilliseconds() + number);
           break;
       }
       return this;
@@ -32,41 +32,48 @@ export const shmoment = initValue => {
     subtract(dateInterval, number) {
       switch (dateInterval) {
         case 'years':
-          resultDate = initValue.setFullYear(initValue.getFullYear() - number);
+          initValue.setFullYear(initValue.getFullYear() - number);
           break;
         case 'months':
-          resultDate = initValue.setMonth(initValue.getMonth() - number);
+          initValue.setMonth(initValue.getMonth() - number);
           break;
         case 'days':
-          resultDate = initValue.setDate(initValue.getDay() - number);
+          initValue.setDate(initValue.getDay() - number);
           break;
         case 'hours':
-          resultDate = initValue.setHours(initValue.getHours() - number);
+          initValue.setHours(initValue.getHours() - number);
           break;
         case 'minutes':
-          resultDate = initValue.setMinutes(initValue.getMinutes() - number);
+          initValue.setMinutes(initValue.getMinutes() - number);
           break;
         case 'seconds':
-          resultDate = initValue.setSeconds(initValue.getSeconds() - number);
+          initValue.setSeconds(initValue.getSeconds() - number);
           break;
         case 'milliseconds':
-          resultDate = initValue.setMilliseconds(initValue.getMilliseconds() - number);
+          initValue.setMilliseconds(initValue.getMilliseconds() - number);
           break;
       }
       return this;
     },
 
     result() {
-      return initValue;
+      return initValue.toLocaleString('en', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      });
     },
   };
   return newValue;
 };
 
 // const res = shmoment(new Date(2020, 0, 7, 17, 17, 17))
-//  .add('minutes', 2)
-//  .add('days', 8)
-//  .subtract('years', 1)
-//  .result(); // ...Jan 15 2019 17:19:17...
+// .add('minutes', 2)
+// .add('days', 8)
+// .subtract('years', 1)
+// .result(); // ...Jan 15 2019 17:19:17...
 
 // console.log(res);
