45914b22c502f0b8d3e1aab1171000bd3f0011e2
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-15 12:15:28 +0300

    test

--- js-full/asynchronous-code/1/index.js
+++ js-full/asynchronous-code/1/index.js
@@ -12,17 +12,17 @@ export const addImage = (imgSrc, callback) => {
   imgElem.addEventListener('error', () => callback('Image load is failed'));
 };
 
-const onImageLoaded = (error, imgElem) => {
+const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
     return;
   }
-  const { width, height } = imgElem;
+  const { width, height } = data;
   const sizeElem = document.querySelector('.image-size');
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-// addImage(
-// 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-//  onImageLoaded,
-// );
+addImage(
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+  onImageLoaded,
+);
