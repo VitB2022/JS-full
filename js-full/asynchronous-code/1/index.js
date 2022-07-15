ca1e47d11cd200182f9ae9957857fc6cf73b59d2
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-15 12:05:20 +0300

    corrected

--- js-full/asynchronous-code/1/index.js
+++ js-full/asynchronous-code/1/index.js
@@ -9,21 +9,20 @@ export const addImage = (imgSrc, callback) => {
     callback(null, { width, height });
   };
   imgElem.addEventListener('load', onImageLoaded);
-  imgElem.addEventListener('error', () => callback('Image load fail'));
+  imgElem.addEventListener('error', () => callback('Image load is failed'));
 };
-const imgSrc =
-  'https://gromcode.s3.eu-central-1.aazonaws.com/front-end/html-css/lesson24/hw1/illustration-window.png';
-const onImageLoaded = (error, imgElem) => {
+
+const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
     return;
   }
-  const { width, height } = imgElem;
+  const { width, height } = data;
   const sizeElem = document.querySelector('.image-size');
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-addImage(
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-  onImageLoaded,
-);
+// addImage(
+//  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+//  onImageLoaded,
+// );
