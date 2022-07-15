16ab30607172b9ee28fb1e4dda7d9b49e16f83d1
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-15 21:55:49 +0300

    added func

---
+++ js-full/promise-handlers/1/index.js
@@ -0,0 +1,24 @@
+const requestUserData = userId => {
+  const request = new Promise((resolve, reject) => {
+    if (userId === 'broken') {
+      setTimeout(() => {
+        reject(new Error('User not found'));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: 'John',
+          age: 17,
+          email: `${userId}@example.com`,
+          userId,
+        });
+      }, 1000);
+    }
+  });
+  return request;
+};
+
+requestUserData('user777')
+  .then(data => console.log(data))
+  .catch(error => console.log(error))
+  .finally(() => console.log('finally'));
