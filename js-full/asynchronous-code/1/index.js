3075b7553252f96d5df6e236a469c29ee00e9a37
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-15 11:59:29 +0300

    test

--- js-full/asynchronous-code/1/index.js
+++ js-full/asynchronous-code/1/index.js
@@ -23,7 +23,7 @@ const onImageLoaded = (error, data) => {
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-//addImage(
-//  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-//  onImageLoaded,
-// );
+addImage(
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+  onImageLoaded,
+);
