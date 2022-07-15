c2b9e372dfaad7f5141575a4ffd0c3737a8bb72b
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-15 11:57:01 +0300

    corrected

--- js-full/asynchronous-code/1/index.js
+++ js-full/asynchronous-code/1/index.js
@@ -1,33 +1,29 @@
 export const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement('img');
-  imgElem.setAttribute('alt', 'My Photo');
-  imgElem.src = imgSrc;
+  imgElem.setAttribute('alt', 'My photo');
   const containerElem = document.querySelector('.page');
+  imgElem.src = imgSrc;
   containerElem.append(imgElem);
-
   const onImageLoaded = () => {
     const { width, height } = imgElem;
     callback(null, { width, height });
   };
   imgElem.addEventListener('load', onImageLoaded);
-  imgElem.addEventListener('error', () => callback('Image load failed'));
+  imgElem.addEventListener('error', () => callback('Image load fail'));
 };
-
-// callack function
+const imgSrc =
+  'https://gromcode.s3.eu-central-1.aazonaws.com/front-end/html-css/lesson24/hw1/illustration-window.png';
 const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
     return;
   }
-
   const { width, height } = data;
   const sizeElem = document.querySelector('.image-size');
-
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-// examples
-addImage(
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-  onImageLoaded,
-);
+//addImage(
+//  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+//  onImageLoaded,
+// );
