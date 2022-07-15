63ac93f179c994ca0289bb86b03313467784c7ff
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-15 16:47:07 +0300

    added promise func

---
+++ js-full/promise/1/index.js
@@ -0,0 +1,25 @@
+export const addImage = imgSrc => {
+  const p = new Promise((resolve, reject) => {
+    const imgElem = document.createElement('img');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
+    imgElem.addEventListener('load', onImageLoaded);
+
+    imgElem.addEventListener('error', () => reject(new Error('Image load is failed')));
+  });
+  return p;
+};
+
+const resultPromise = addImage(
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+);
+
+resultPromise.then(data => console.log(data));
+
+console.log(resultPromise);
