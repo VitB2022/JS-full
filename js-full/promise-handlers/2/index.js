2920684cc3a8dc9a6d173ddbff773a22f07cc65e
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-16 13:37:36 +0300

    corrected messages in console log

--- js-full/promise-handlers/2/index.js
+++ js-full/promise-handlers/2/index.js
@@ -10,14 +10,14 @@ export const asyncCalculator = numbr =>
         new Promise(resolve => {
           setTimeout(() => {
             const result = value * value;
-            console.log(`Squared value : ${result}`);
+            console.log(`Squared value: ${result}`);
             resolve(result);
           }, 3000);
         }),
     )
     .then(value => {
       const result = value * 2;
-      console.log(`Doubled value : ${result}`);
+      console.log(`Doubled value: ${result}`);
       return result;
     });
 
