d40d0cae56bf85f60caa131d3e55398f8e0a13fd
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-21 13:44:17 +0300

    test

--- js-full/error-handling/2/index.js
+++ js-full/error-handling/2/index.js
@@ -13,11 +13,10 @@ const failRequest = Promise.reject(new Error('Something went wrong'));
 failRequest
   .catch(function onError2(error) {
     console.error('onError2', error.message);
-    throw error;
   })
   .then(function onSuccess2(data) {
     console.log('onSuccess2', data);
   })
   .catch(function onError3(error) {
-    console.log('Server error', error.message);
+    console.error('onError3', error.message);
   });
