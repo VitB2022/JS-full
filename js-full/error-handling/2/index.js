22c55f7b1f1905da89eae9f3c54278378267b3e0
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-21 13:12:50 +0300

    added code

---
+++ js-full/error-handling/2/index.js
@@ -0,0 +1,25 @@
+const successRequest = Promise.resolve({ name: 'Tom' });
+
+successRequest
+  .then(data => {
+    console.log(data);
+    throw new Error('Unexpected error');
+  })
+  .catch(err => {
+    console.warn(err.message);
+  });
+
+const failedRequest = Promise.reject(new Error('Fail'));
+
+failedRequest
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.warn(err.message);
+    // return Promise.reject(err);
+    throw err;
+  })
+  .then(data => {
+    console.log(data);
+  });
