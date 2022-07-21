12234c38d57a4edd51cba3c23793b5bf54f6cc7f
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-21 13:20:45 +0300

    corrected

--- js-full/error-handling/2/index.js
+++ js-full/error-handling/2/index.js
@@ -1,25 +1,23 @@
 const successRequest = Promise.resolve({ name: 'Tom' });
 
-// successRequest
-//   .then(data => {
-//     console.log(data);
-//     throw new Error('Unexpected error');
-//   })
-//   .catch(err => {
-//     console.warn(err.message);
-//   });
-
-const failedRequest = Promise.reject(new Error('Fail'));
-
-failedRequest
-  .then(data => {
-    console.log(data);
+successRequest
+  .then(function onSuccess1(data) {
+    throw new Error('Unexpected error');
   })
-  .catch(err => {
-    console.warn(err.message);
-    // return Promise.reject(err);
+  .catch(function onError1(error) {
+    console.error('onError1', error.message);
+  });
+
+const failRequest = Promise.reject(new Error('Something went wrong'));
+
+failRequest
+  .catch(function onError2(error) {
+    console.error('onError2', error.message);
     throw err;
   })
-  .then(data => {
-    console.log(data);
+  .then(function onSuccess2(data) {
+    console.log('onSuccess2', data);
+  })
+  .catch(function onError3(error) {
+    console.error('onError3', error.message);
   });
