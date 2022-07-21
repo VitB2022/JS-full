ef1a62d5941a699657c73f1ab1dea7aa72d27dad
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-21 20:00:04 +0300

    corrected url

--- js-full/async-await/1/index.js
+++ js-full/async-await/1/index.js
@@ -1,6 +1,6 @@
 export const fetchUser = async userId => {
   try {
-    const response = await fetch(`https://ape.github.com/users/${userId}`);
+    const response = await fetch(`https://api.github.com/users/${userId}`);
     if (!response.ok) {
       return null;
     }
